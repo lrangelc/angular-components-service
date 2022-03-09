6cba01cb3a264d0a7ef2c4b866086e13cb93f4b7
Luis Rangel <luisrangelc@gmail.com>
2022-03-09 05:09:31 -0600

    feat: pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -14,44 +14,8 @@ export class ProductsComponent implements OnInit {
 
   myShoppingCart: Product[] = [];
   products: Product[] = []
-  // products: Product[] = [
-  //   {
-  //     id: '1',
-  //     name: 'EL mejor juguete',
-  //     price: 565,
-  //     image: './assets/images/toy.jpg'
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Bicicleta casi nueva',
-  //     price: 356,
-  //     image: './assets/images/bike.jpg'
-  //   },
-  //   {
-  //     id: '3',
-  //     name: 'Colleción de albumnes',
-  //     price: 34,
-  //     image: './assets/images/album.jpg'
-  //   },
-  //   {
-  //     id: '4',
-  //     name: 'Mis libros',
-  //     price: 23,
-  //     image: './assets/images/books.jpg'
-  //   },
-  //   {
-  //     id: '5',
-  //     name: 'Mis vasos',
-  //     price: 10,
-  //     image: './assets/images/glasses.jpg'
-  //   },
-  //   {
-  //     id: '6',
-  //     name: 'Mi casa',
-  //     price: 2000,
-  //     image: './assets/images/house.jpg'
-  //   }
-  // ];
+  today = new Date();
+  lastDate = new Date(2021, 1, 21);
 
   constructor(private storeService: StoreService,
     private productsService: ProductsService) {
